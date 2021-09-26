a7146b047de276ee51f0d2dad2d9d0547da34d11
Joelle Plow <joelleplow@aol.com>
2021-09-26 15:31:26 +0100

    Break parsing into modules

--- src/parse/parse.h
+++ src/parse/parse.h
@@ -3,54 +3,13 @@
 #include <stdlib.h>
 #include <vector>
 
+#include "./utils.h"
+
 #define tabWidth 2
 
-const char* endOfString(const char* str) {
-  while(*str)
-    ++str;
-  return str;
-}
 
-const char *endOfLine(const char *str) {
-  if(*str == '\0')
-    return nullptr;
-  do {
-    str++;
-  } while (*str && str[0] != '\n');
-  return str+1;
-}
 
-//char* newString(char* str) {
-//}
-const char *newString(const char *str, int length) {
-  char *mem = (char*)malloc(length);
-  memcpy(mem, str, length + 1);
-  mem[length] = '\0';
-  return mem;
-}
 
-const char* newString(const char* start, const char* end) {
-  return newString(start, end-start);
-}
-
-const char *copyLine(const char *str) { 
-  return newString(str, endOfLine(str)); 
-}
-
-void forEachLine(const char *str, void (*f)(const char *line)) {
-  const char *end;
-  do {
-    end = endOfLine(str);
-    f(newString(str, end));
-    str = end;
-  } while (*str);
-}
-
-bool isBlankLine(const char *str) {
-  while (*str == ' ' || *str == '\t')
-    ++str;
-  return *str == '\n';
-}
 
 short int measureIndent(const char *str) {
   short int indent = 0;
@@ -125,15 +84,18 @@ void forEachIndentedSection(
   } while (*str && measureIndent(str) == indent);
 }
 
-const char* endOfInlineWhitespace(const char* str) {
-  while(*str == ' ' || *str == '\t')
-    ++str;
-  return str;
-}
+typedef struct {
+  const char* header;
+  const char* body;
+} IndentedSection;
 
-const char* endOfWhitespace(const char* str) {
-  while(*str == ' ' || *str == '\t' || *str == '\n')
-    ++str;
-  return str;
-}
+IndentedSection parseIndentedSection(char*   start) {
+  const char* end = endOfIndentedSection(start);
+  const char* headerEnd = endOfLine(start);
 
+  IndentedSection out = {newString(start, headerEnd),
+   newString(headerEnd, end)
+  };
+
+  return out;
+}
