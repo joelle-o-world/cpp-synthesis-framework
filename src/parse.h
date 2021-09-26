d3acc0e05f2d9cafba3e08de03201e74ba6261f2
Joelle Plow <joelleplow@aol.com>
2021-09-26 13:17:48 +0100

    Parcel out attribute functions to separate module

--- src/parse.h
+++ src/parse.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdlib.h>
 #include <vector>
 
@@ -135,37 +137,3 @@ const char* endOfWhitespace(const char* str) {
   return str;
 }
 
-const char* startOfAttributeName(const char* str) {
-  return endOfInlineWhitespace(str);
-}
-
-const char* endOfAttributeName(const char* str) {
-  const char* end = str;
-  while(*end != ':')
-    if(*end == '\n' || !*end)
-      return nullptr;
-
-  // Otherwise
-  return end;
-}
-
-const char* copyAttributeName(const char* str) {
-  const char* start = startOfAttributeName(str);
-  const char* end = endOfAttributeName(str);
-  return newString(start, end);
-}
-
-const char* startOfAttributeValue(const char* startOfLine) {
-  return endOfWhitespace(endOfAttributeName(startOfLine) + 1);
-}
-
-const char* endOfAttributeValue(const char* startOfLine) {
-  return endOfIndentedSection(startOfLine);
-}
-
-const char* copyAttributeValue(const char* startOfLine) {
-  return newString(startOfAttributeValue(startOfLine), endOfAttributeValue(startOfLine));
-}
-
-
-// TODO: attributeValue
