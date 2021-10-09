e5e29fb93a0f2501b87a0e594499a12a80bdfd02
Joelle Plow <joelleplow@aol.com>
2021-10-09 09:32:16 +0100

    refactor FTM processor

--- src/processes/FTM.h
+++ src/processes/FTM.h
@@ -5,30 +5,25 @@
 #include "../AudioProcess.h"
 
 class FTM : public AudioProcess {
-  public:
-    FTM():AudioProcess(1,1) { }
+public:
+  FTM() : AudioProcess(1, 1) {}
 
-    void process() override {
-      SignalBuffer& in = *inputs[0];
-      SignalBuffer& out = *outputs[0];
+  void process() override { process(*inputs[0], *outputs[0]); }
 
-      for(int i=0; i < signalChunkSize; ++i)
-        out[i] = 440.0 * pow(2.0, (in[i] - 69.0) / 12.0);
-    }
+  void process(SignalBuffer &in, SignalBuffer &out) {
+    for (int i = 0; i < signalChunkSize; ++i)
+      out[i] = 440.0 * pow(2.0, (in[i] - 69.0) / 12.0);
+  }
 };
 
 class SemitoneToRatio : public AudioProcess {
-  public:
-    SemitoneToRatio(): AudioProcess(1,1) {
-      inputs = new SignalBuffer*[1];
-      outputs = new SignalBuffer*[1];
-    }
+public:
+  SemitoneToRatio() : AudioProcess(1, 1) {}
 
-    void process() override {
-      SignalBuffer& in = *inputs[0];
-      SignalBuffer& out = *outputs[0];
+  void process() override { process(*inputs[0], *outputs[0]); }
 
-      for(int i=0; i < signalChunkSize; ++i)
-        out[i] = pow(2.0, in[i] / 12.0);
-    }
+  void process(SignalBuffer &in, SignalBuffer &out) {
+    for (int i = 0; i < signalChunkSize; ++i)
+      out[i] = pow(2.0, in[i] / 12.0);
+  }
 };
