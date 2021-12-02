1ccf85a12adc035be4f192015402f856d0a77fac
Joelle Plow <joelleplow@aol.com>
2021-12-02 17:40:18 +0100

    update processes that were using old pattern for io

--- src/processes/GlitchyLoop.h
+++ src/processes/GlitchyLoop.h
@@ -5,48 +5,44 @@
 #include "./Osc.h"
 #include "./arithmetic.h"
 
+class GlitchyLoop : public AudioProcess {
+public:
+  StereoBuffer internalBuffer;
+  Osc osc1;
+  Multiply multiply;
 
-class GlitchyLoop: public AudioProcess {
-  public: 
-    StereoBuffer internalBuffer;
-    Osc osc1;
-    Multiply multiply;
+  GlitchyLoop() : AudioProcess(1, 1) {
+    initialiseBuffer(sineWavetable, 10, .2);
+  }
 
-    GlitchyLoop(): AudioProcess(1, 1) {
-      initialiseBuffer(sineWavetable, 10, .2);
+  void initialiseBuffer(Wavetable &wavetable, float frequency,
+                        float phaseShift) {
+    float phase = phaseShift;
+    Osc singleUseOsc;
+    singleUseOsc.setPhase(phaseShift);
+    singleUseOsc.process(frequency, internalBuffer);
+  }
 
+  void processStatefully() {
+    TypedSignalBuffer &f = *inputs[0].buffer, &out = *outputs[0].buffer;
+
+    if (out.type != Stereo)
+      throw "output must be stereo";
+
+    else {
+      if (f.type == Stereo)
+        process(*f.stereo, *out.stereo);
+      else if (f.type == Mono)
+        process(*f.mono, *out.stereo);
+      else if (f.type == Constant)
+        process(*f.constant, *out.stereo);
+      else
+        throw "Unexpected io";
     }
+  }
 
-    void initialiseBuffer(Wavetable& wavetable, float frequency, float phaseShift) {
-      float phase = phaseShift;
-      Osc singleUseOsc;
-      singleUseOsc.setPhase(phaseShift);
-      singleUseOsc.process(frequency, internalBuffer);
-    }
-
-    void processStatefully() {
-      TypedSignalBuffer &f = *inputs[0], 
-                        &out = *outputs[0];
-
-      if(out.type != Stereo)
-        throw "output must be stereo";
-
-      else {
-        if(f.type == Stereo)
-          process(*f.stereo, *out.stereo);
-        else if(f.type == Mono)
-          process(*f.mono, *out.stereo);
-        else if(f.type == Constant)
-          process(*f.constant, *out.stereo);
-        else
-         throw "Unexpected io";
-      }
-
-    }
-
-    template<typename Frequency>
-    void process(Frequency& f, StereoBuffer& out) {
-      osc1.process(f, out);
-      multiply.process(internalBuffer, out, out);
-    }
+  template <typename Frequency> void process(Frequency &f, StereoBuffer &out) {
+    osc1.process(f, out);
+    multiply.process(internalBuffer, out, out);
+  }
 };
