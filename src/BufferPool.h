4cd6146feea9732ac699e996bd6b3ee74d29ce49
Joelle Plow <joelleplow@aol.com>
2021-12-19 23:05:15 +0000

    trivial logging changes

--- src/BufferPool.h
+++ src/BufferPool.h
@@ -13,20 +13,23 @@ public:
     if (busy == a.size()) {
       std::cout << "Creating a new buffer.\n";
       T *newbuf = new T[size];
+      std::cout << "Created new buffer (address = " << &newbuf << ") \n";
       a.push_back(newbuf);
+      std::cout << "Added buffer to the array\n";
     }
     n = busy;
     ++busy;
     std::cout << "Returning buffer " << n << " of " << a.size() << "\n";
-    std::cout << "Buffer address: " << &a[n] << "\n";
+    std::cout << "Buffer address: " << a[n] << "\n";
     return a[n];
   }
 
   void release(int n) {
     std::cout << "releasing " << n << ", there are now " << busy
               << " busy buffers\n";
+    std::cout << "Swapping " << (busy - 1) << " with " << n << "\n";
     --busy;
-    auto tmp = a[busy];
+    T *tmp = a[busy];
     a[busy] = a[n];
     a[n] = tmp;
     // swap(a[n], a[--busy]);
